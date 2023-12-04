4bad701ebd0d48c031865ae6f2a494989df5de5b
“San” <santiagoag365gmail.com>
2023-12-03 20:02:00 -0500

    Se añadió funcionalidad al modal para añadir tareas, también se dieron estilos

---
+++ js/modal-to-do.js
@@ -0,0 +1,22 @@
+const modalToDo= document.querySelector('.modal-to-do');
+const btnToDo= document.querySelector('.btn-to-do');
+const overlay= document.querySelector('.overlay');
+const closeBtn= document.querySelector('.btn-close')
+
+btnToDo.onclick = function() {
+    modalToDo.style.display = "block";
+    overlay.style.display = "block";
+  };
+
+  closeBtn.onclick= closeModal;
+
+  function closeModal() {
+    modalToDo.style.display= 'none';
+    overlay.style.display= 'none';
+  };
+
+  window.onclick = function(event) {
+    if (event.target == overlay) {
+      closeModal();
+    }
+  };
